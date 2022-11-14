372bc01d2aac978fa8a898a830779ae0a77c4d0e
LaneSandburg <lane.sandburg@gmail.com>
2022-11-14 12:40:20 -0600

    Added image swaping and new

---
+++ BuildingTracker/wwwroot/js/Map.js
@@ -0,0 +1,19 @@
+﻿/// <reference path="jquery.rwdimagemaps.js" />
+var flip = true;
+function changeImage(UL, LL) {
+    
+    if (flip == true) {
+        document.getElementById("img2").src = UL;
+        document.getElementById("img1").src = LL;
+        rwdImageMaps();
+        flip = false;
+    } else{
+        document.getElementById("img1").src = UL;
+        document.getElementById("img2").src = LL;
+        rwdImageMaps();
+        flip = true;
+    }
+    
+} $(document).ready(function () {
+    $('img[usemap]').rwdImageMaps();
+});
